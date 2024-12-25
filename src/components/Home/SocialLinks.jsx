f006656cae6b2e81419fda83e6a36213495a9ddf
viek4nag <vivek4nag@gmail.com>
2024-12-25 23:46:35 +0530

    social links added

---
+++ src/components/Home/SocialLinks.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable react/prop-types */
+
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+
+const SocialLinks = ({ data, index }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    return (
+        <motion.a
+            key={index}
+            href={data.url} 
+            target="_blank" 
+            className="relative w-8 h-8 md:w-12 md:h-12 rounded-full bg-gradient-to-br from-primary to-secondary p-[2px] flex items-center justify-center cursor-pointer"
+            onMouseEnter={()=> setIsHovered(true)}
+            onMouseLeave={(() => setIsHovered(false))}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{delay: index * 0.2}}
+        >
+            <AnimatePresence>
+                {isHovered && (
+                    <motion.div
+                        className={` absolute inset-2 blur-md bg-gradient-to-br from-primary to-secondary transition-all duration-500 -z-10`}
+                    ></motion.div>
+                )}
+
+                <div className="w-full h-full rounded-full bg-bgPrimary flex items-center justify-center">
+                    <data.Icon className={`text-textlight`} />
+                </div>
+            </AnimatePresence>
+        </motion.a>
+    );
+};
+
+export default SocialLinks;
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,28 +1,57 @@
-import { faHouse,faComputer, faGraduationCap, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faComputer, faGraduationCap, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FaLinkedinIn, FaGithub, FaTelegram, FaTwitter } from "react-icons/fa";
 
 export const Menus = [
-    {
-      id: `home-${Date.now()}`,
-      Icon: faHouse,
-      url: "#home",
-      name: "Home",
-    },
-    {
-      id: `skills-${Date.now()}`,
-      Icon: faGraduationCap,
-      url: "#skills",
-      name: "Skills",
-    },
-    {
-      id: `projects-${Date.now()}`,
-      Icon: faComputer,
-      url: "#projects",
-      name: "Projects",
-    },
-    {
-      id: `contact-${Date.now()}`,
-      Icon: faEnvelope,
-      url: "#contact",
-      name: "Contact",
-    },
-  ];
+  {
+    id: `home-${Date.now()}`,
+    Icon: faHouse,
+    url: "#home",
+    name: "Home",
+  },
+  {
+    id: `skills-${Date.now()}`,
+    Icon: faGraduationCap,
+    url: "#skills",
+    name: "Skills",
+  },
+  {
+    id: `projects-${Date.now()}`,
+    Icon: faComputer,
+    url: "#projects",
+    name: "Projects",
+  },
+  {
+    id: `contact-${Date.now()}`,
+    Icon: faEnvelope,
+    url: "#contact",
+    name: "Contact",
+  },
+];
+
+export const Socials = [
+  
+  {
+    id: `linkedin-${Date.now()}`,
+    Icon: FaLinkedinIn,
+    url: "https://www.linkedin.com/in/vivek-nag-a125482b2/",
+    color: "#0072b1",
+  },
+  {
+    id: `github-${Date.now()}`,
+    Icon: FaGithub,
+    url: "https://github.com/vivek4nag",
+    color: "#fff",
+  },
+  {
+    id: `twitter-${Date.now()}`,
+    Icon: FaTwitter,
+    url: "https://x.com/vivek4nag",
+    color: "#ff0000",
+  },
+  {
+    id: `telegram-${Date.now()}`,
+    Icon: FaTelegram,
+    url: "https://t.me/Norindar_Mudi",
+    color: "#ff0000",
+  },
+];
