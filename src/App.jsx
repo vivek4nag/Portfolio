e2fd79c27469038dd046477d9c3ea7d8a68fa659
viek4nag <vivek4nag@gmail.com>
2024-12-29 19:22:38 +0530

    almost all section done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@
 import Home from "./Pages/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Skills from "./Pages/Skills";
+// import Projects from "./Pages/Projects";
+import Contact from "./Pages/ContactMe";
 import Projects from "./Pages/Projects";
+// import FooterCard from "./Pages/Footer";
 import { StarsBackground } from "./components/ui/stars-background";
 import { ShootingStars } from "./components/ui/shooting-stars";
 
@@ -11,29 +14,32 @@ import { ShootingStars } from "./components/ui/shooting-stars";
 export default function App() {
   return (
     <div className="relative min-h-screen">
-      <StarsBackground className="fixed top-0 left-0 w-full h-full -z-10"/>
+      <StarsBackground className="fixed top-0 left-0 w-full h-full -z-10"/> 
       <ShootingStars className="fixed top-0 left-0 w-full h-full -z-10"/>
-    <div className=" w-full xl:w-[1400px] py-32 px-4 lg:px-12 pr-4 lg:pr-32">
-      {/* <ParticleBackground  /> */}
-      
+      <div className=" w-full py-28 md:px-4 lg:px-12  lg:pr-32">
+        {/* <ParticleBackground  /> */}
 
-      {/* Navbar */}
-      <Navbar />
 
-      {/* home container */}
-      <Home/> 
+        {/* Navbar */}
+        <Navbar />
 
-      {/* skills container */}
-      <Skills/>
+        {/* home Page */}
+        <Home />
 
-      {/* projects container */}
-      <Projects/>
+        {/* skills Page */}
+        <Skills />
 
-      {/* contact container */}
+        {/* projects Page */}
+        <Projects/>
 
-      {/* footer */}
+        {/* contact Page */}
+        <Contact />
 
-    </div>
+
+        {/* footer */}
+        {/* <FooterCard/> */}
+
+      </div>
     </div>
 
   );
