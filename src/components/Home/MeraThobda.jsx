27c626894b1a7e36814f8c3dfb388efce25276fb
viek4nag <vivek4nag@gmail.com>
2024-12-31 00:22:41 +0530

    images attached

--- src/components/Home/MeraThobda.jsx
+++ src/components/Home/MeraThobda.jsx
@@ -1,12 +1,21 @@
-import dp from "../../assets/dp3.jpg";
+import dp1 from "../../assets/dp1.jpg";
+import dp2 from "../../assets/dp2.jpg";
+import dp3 from "../../assets/dp3.jpg";
+import dp4 from "../../assets/dp4.jpg";
+import dp5 from "../../assets/dp5.jpg";
 import { motion } from "framer-motion";
 const MeraThobda = () => {
+
+    let dp = [dp1, dp2, dp3, dp4, dp5];
+    let randomIdx = Math.floor(Math.random() * dp.length);
+    // console.log(randomIdx);
+
     return (
         <>
             <div className="w-full md:w-1/3 h-full flex justify-center items-start md:items-center">
                 <motion.img
-                    src={dp}
-                    className="w-[220px] md:w-[280px] h-auto object-contain rounded-full cursor-pointer scale-100"
+                    src={dp[randomIdx]}
+                    className="w-[200px] md:w-[240px] h-auto object-contain rounded-full cursor-pointer scale-100"
                     initial={{ y: 0 }}
                     animate={{
                         x: [-4, 4, -4],
@@ -26,7 +35,7 @@ const MeraThobda = () => {
                 />
             </div>
         </>
-    )
-}
+    );
+};
 
-export default MeraThobda
+export default MeraThobda;
